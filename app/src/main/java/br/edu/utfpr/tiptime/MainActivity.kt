ba6cefedd11296cc15020f66bec5c9e381ee9d11
robisoncrisbrito <robison.brito@gmail.com>
2024-10-31 21:13:05 -0300

    .

--- app/src/main/java/br/edu/utfpr/tiptime/MainActivity.kt
+++ app/src/main/java/br/edu/utfpr/tiptime/MainActivity.kt
@@ -1,20 +1,59 @@
 package br.edu.utfpr.tiptime
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import br.edu.utfpr.tiptime.databinding.ActivityMainBinding
+import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate( layoutInflater )
+        setContentView( binding.root )
+
+        if ( savedInstanceState != null ) {
+            val tip = savedInstanceState.getString( "tip" )
+            binding.tipResult.text = tip
         }
+
+        binding.calculateButton.setOnClickListener {
+            calculateButtonOnClick()
+        }
+    }
+
+    private fun calculateButtonOnClick() {
+
+        val stringInTextField = binding.costOfService.text.toString()
+        val cost = stringInTextField.toDoubleOrNull() ?: return
+
+        val selectedId = binding.tipOption.checkedRadioButtonId
+
+        val tipPercentage = when ( selectedId ) {
+            R.id.option_twenty_percent -> 0.2
+            R.id.option_eighteen_percent -> 0.18
+            else -> 0.15
+        }
+
+        var tip = cost * tipPercentage
+
+        val roundUp = binding.roundUpSwitch.isChecked
+
+        if ( roundUp ) {
+            tip = kotlin.math.ceil( tip )
+        }
+
+        val formattedTip = NumberFormat.getCurrencyInstance().format ( tip )
+
+        binding.tipResult.text = getString( R.string.tip_amount, formattedTip )
+
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString( "tip", binding.tipResult.text.toString() )
     }
 }
